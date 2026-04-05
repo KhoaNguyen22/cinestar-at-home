909e0c3dea2f36507e2bd9f3046984a9ba600042
Khoa <khoawh23@gmail.com>
2026-04-05 10:27:04 +0700

    Setup back end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import HeaderLogo from "./assets/header-logo.webp";
+import SearchIcon from "./assets/ic-header-search.svg";
+import AuthIcon from "./assets/ic-header-auth.svg";
+import VietNamIcon from "./assets/footer-vietnam.svg";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
