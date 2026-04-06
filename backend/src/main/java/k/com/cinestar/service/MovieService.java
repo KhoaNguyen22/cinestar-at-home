7704aaec77e80041d7f2608b62327b70232c5e00
Khoa <khoawh23@gmail.com>
2026-04-06 13:45:30 +0700

    chore(backend/movie) : apply spotless formatting

--- backend/src/main/java/k/com/cinestar/service/MovieService.java
+++ backend/src/main/java/k/com/cinestar/service/MovieService.java
@@ -1,22 +1,23 @@
 package k.com.cinestar.service;
 
-import k.com.cinestar.entity.Movie;
-import k.com.cinestar.repository.MovieRepository;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import k.com.cinestar.entity.Movie;
+import k.com.cinestar.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
     private final MovieRepository movieRepository;
-    public List <Movie> findAllMovies (){
+
+    public List<Movie> findAllMovies() {
         return movieRepository.findAll();
     }
-    public void addMovie(Movie movie){
+
+    public void addMovie(Movie movie) {
         movieRepository.save(movie);
     }
 }
