562397b6703b1db1412e444542b0590529df8c9f
Khoa <khoawh23@gmail.com>
2026-04-06 13:44:00 +0700

    refactor(backend/movie) : change MovieController request mapping singular to plural

--- backend/src/main/java/k/com/cinestar/controller/MovieController.java
+++ backend/src/main/java/k/com/cinestar/controller/MovieController.java
@@ -1,21 +1,23 @@
 package k.com.cinestar.controller;
 
-import k.com.cinestar.entity.Movie;
-import k.com.cinestar.service.MovieService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import k.com.cinestar.entity.Movie;
+import k.com.cinestar.service.MovieService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/movie")
+@RequestMapping("/api/movies")
 @RequiredArgsConstructor
 public class MovieController {
     private final MovieService movieService;
+
     @GetMapping
-    public List<Movie> getAllMovies(){
+    public List<Movie> getAllMovies() {
         return movieService.findAllMovies();
     }
 }
