60b7f9a8e83914c56681dc905ec89db965b1bfec
Khoa <khoawh23@gmail.com>
2026-06-09 20:46:35 +0700

    refactor(backend/movie) : change collections to lazy loading

--- backend/src/main/java/k/com/cinestar/entity/Movie.java
+++ backend/src/main/java/k/com/cinestar/entity/Movie.java
@@ -2,6 +2,7 @@ package k.com.cinestar.entity;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import lombok.AccessLevel;
@@ -21,13 +22,18 @@ public class Movie {
     String posterUrl;
     String title;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "movie_genre",
             joinColumns = @JoinColumn(name = "movie_id", foreignKey = @ForeignKey(name = "fk_movie_genre_movie")),
             inverseJoinColumns =
                     @JoinColumn(name = "genre_id", foreignKey = @ForeignKey(name = "fk_movie_genre_genre")))
     Set<Genre> genres;
+
+
+    @OneToMany(mappedBy = "movie",fetch =  FetchType.LAZY)
+     @JsonManagedReference
+    List<Show> shows;
 
     int duration;
     String country;
